6f37472f80966bd8bcc6c2f30ac49fee56b234ce
USER <hameed.momin@gmail.com>
2022-02-17 11:00:55 +0530

    terraform

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -2,7 +2,6 @@ resource "aws_spot_instance_request" "sample" {
   ami            = "ami-0d997c5f64a74852c"
   spot_price     = "0.03"
   instance_type  = "t2.micro"
-  key_name       = "Name"
 
   tags = {
     Name  = "new"
@@ -11,4 +10,9 @@ resource "aws_spot_instance_request" "sample" {
 
 provider "aws" {
   region    = "us-east-1"
+}
+resource "aws_ec2_tag" "sample" {
+  key              = "Name"
+  resource_id      = aws_spot_instance_request.sample.id
+  value            = "spot"
 }
