83b06a8ece64fa3b13e04dc356b01dfb1ccbecaf
USER <hameed.momin@gmail.com>
2022-02-17 10:52:47 +0530

    terraform

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -2,6 +2,7 @@ resource "aws_spot_instance_request" "sample" {
   ami            = "ami-0d997c5f64a74852c"
   spot_price     = "0.03"
   instance_type  = "t2.micro"
+  key_name       = "Name"
 
   tags = {
     Name  = "new"
